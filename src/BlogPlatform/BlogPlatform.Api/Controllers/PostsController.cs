0c7bb2301f4feace8157a4ea1d5b87266f3dfeb0
michalantolik <michal.antolik@gmail.com>
2026-05-02 04:43:28 +0200

    Add posts API contract and endpoint

---
+++ src/BlogPlatform/BlogPlatform.Api/Controllers/PostsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlogPlatform.Application.Posts;
+
+namespace BlogPlatform.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PostsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<PostDto>> Get()
+    {
+        var posts = new List<PostDto>
+        {
+            new PostDto
+            {
+                Id = 1,
+                Title = "First post",
+                Content = "Hello from .NET API"
+            }
+        };
+
+        return Ok(posts);
+    }
+}
---
+++ src/BlogPlatform/BlogPlatform.Application/Posts/PostDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BlogPlatform.Application.Posts;
+
+public class PostDto
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Content { get; set; } = string.Empty;
+}
