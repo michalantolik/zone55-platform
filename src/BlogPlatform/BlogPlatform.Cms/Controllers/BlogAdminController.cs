9fb383a67654a19031310b727b76421b688135c4
michalantolik <michal.antolik@gmail.com>
2026-05-08 19:11:33 +0200

    Build blog admin workspace

---
+++ src/BlogPlatform/BlogPlatform.Cms/Controllers/BlogAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPlatform.Cms.Controllers;
+
+public sealed class BlogAdminController : Controller
+{
+    [HttpGet("/blog-admin")]
+    public IActionResult Index()
+    {
+        return View("~/Views/BlogAdmin/Index.cshtml");
+    }
+}
