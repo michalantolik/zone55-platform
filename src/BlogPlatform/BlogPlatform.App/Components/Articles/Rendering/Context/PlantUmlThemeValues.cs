a3a691e7f1612e6e327cc788dbe3980bb371da0c
michalantolik <michal.antolik@gmail.com>
2026-07-10 01:58:44 +0200

    Remove legacy article renderer

--- src/BlogPlatform/BlogPlatform.App/Components/Articles/Rendering/Context/PlantUmlThemeValues.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlogPlatform.App.Components.Articles.Rendering.Context;
-
-public sealed record PlantUmlThemeValues(
-    string Line,
-    string NodeText,
-    string NodeBorder,
-    string NodeBg,
-    string FontFamily);
--- src/BlogPlatform/BlogPlatform.App/Components/Articles/Rendering/Provider/IArticleBlockRenderStrategyProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using BlogPlatform.App.Components.Articles.Rendering.Strategy;
-using BlogPlatform.Contracts.Posts;
-
-namespace BlogPlatform.App.Components.Articles.Rendering.Provider;
-
-public interface IArticleBlockRenderStrategyProvider
-{
-    IArticleBlockRenderStrategy GetStrategy(ArticleBlockType type);
-}
