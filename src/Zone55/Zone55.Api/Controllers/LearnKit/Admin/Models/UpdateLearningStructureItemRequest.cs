fd90dd0edb9543255f1f42d0bf8492aecc613062
michalantolik <michal.antolik@gmail.com>
2026-07-26 17:03:31 +0200

    Fix learning step update validation

--- src/Zone55/Zone55.Api/Controllers/LearnKit/Admin/Models/UpdateLearningStructureItemRequest.cs
+++ src/Zone55/Zone55.Api/Controllers/LearnKit/Admin/Models/UpdateLearningStructureItemRequest.cs
@@ -3,5 +3,5 @@ using System.ComponentModel.DataAnnotations;
 namespace Zone55.Api.Controllers.LearnKit.Admin.Models;
 
 public sealed record UpdateLearningStructureItemRequest(
-    [property: Required, MaxLength(200)] string Title,
-    [property: MaxLength(2000)] string? Summary);
+    [param: Required, MaxLength(200)] string Title,
+    [param: MaxLength(2000)] string? Summary);
